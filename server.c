aa6419ea8588d73c596d98753260bb6b7b60fd69
sebastianrodriguez <sebrod35@purdue.edu>
2025-04-19 01:21:47 -0400

    first commit

---
+++ server.c
@@ -0,0 +1,12 @@
+// Personal Web Server to host my personal website
+
+// Start of all of the necessary include -- will be built further on to it
+
+#include <stdio.h>
+#include <sys/socket.h>
+
+// testing connection acceptance
+
+
+int main(int argc, char **argv) {
+} /* main() */
